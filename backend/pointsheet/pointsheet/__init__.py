22e2dd8df6a5e9747bd817e1f55ca6ef683387fa
Ndifreke Ekott <ndy40.ekott@gmail.com>
2024-12-10 20:44:24 +0000

    feat: Add favicon.ico for api docs [Temp]

--- backend/pointsheet/pointsheet/__init__.py
+++ backend/pointsheet/pointsheet/__init__.py
@@ -1,10 +1,15 @@
 import os
+from pathlib import Path
 
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
+root_dir = os.path.join(Path(__file__).parent.parent)
+
+static_directory = os.path.join(root_dir, 'static')
+template_directory = os.path.join(root_dir, 'templates')
 
 def create_app(test_config=None):
-    app = Flask(__name__)
+    app = Flask(__name__, static_folder=static_directory, template_folder=template_directory,)
 
     app.config.from_mapping(
         SECRET_KEY='dev',
