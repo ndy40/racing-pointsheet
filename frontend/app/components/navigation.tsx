a9f7c20d3e217cde12fd72c58f67a9b8adf50fd6
Ndifreke Ekott <ndy40.ekott@gmail.com>
2025-02-25 11:51:34 +0000

    add assets directory

--- frontend/app/components/navigation.tsx
+++ frontend/app/components/navigation.tsx
@@ -1,4 +1,4 @@
-import logo from './pointsheet.png';
+import logo from '../assets/pointsheet.png';
 import {
     NavigationMenu, NavigationMenuContent, NavigationMenuIndicator,
     NavigationMenuItem,
