1e947ab4378cadfd807ba6e7337f74d359784238
ndy40 <ndy40.ekott@gmail.com>
2025-05-21 09:03:47 +0100

    refactor: Rename Account/entity/Driver to Account/Entity/User
    - Update all references to Account/Driver to Account/User

---
+++ backend/pointsheet/modules/account/queries/get_user.py
@@ -0,0 +1,15 @@
+from lato import Query
+
+from modules.account import account_module
+from modules.account.repository import UserRepository
+from pointsheet.domain import EntityId
+
+
+class GetUser(Query):
+    user_id: EntityId
+
+
+@account_module.handler(GetUser)
+def get_user(cmd: GetUser, repo: UserRepository):
+    user = repo.find_by_id(cmd.user_id)
+    return user
